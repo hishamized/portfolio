1181419cddb85ee9ac52ed4530679ebc8dc3a813
hishamized <mirhashim2001@gmail.com>
2023-10-27 20:32:19 +0530

    Portfolio basic folder structure initialized

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+// scripts.js
+// Load header and footer components
+fetch('components/header.html')
+    .then(response => response.text())
+    .then(data => document.getElementById('header').innerHTML = data);
+
+fetch('components/footer.html')
+    .then(response => response.text())
+    .then(data => document.getElementById('footer').innerHTML = data);
