5861f55afc31e9d8816f125896956c7f1517493b
hishamized <mirhashim2001@gmail.com>
2023-10-28 17:33:13 +0530

    About, Contact and Skills Pages added

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,22 @@
 // scripts.js
+function calculateAge(){
+  // Calculate age
+  var birthDate = new Date("2001-01-22"); // Replace with your birthdate in the 'YYYY-MM-DD' format
+  var currentDate = new Date();
+  var age = currentDate.getFullYear() - birthDate.getFullYear();
+
+  // Check if the birthday hasn't occurred this year yet
+  if (
+    currentDate.getMonth() < birthDate.getMonth() ||
+    (currentDate.getMonth() === birthDate.getMonth() &&
+      currentDate.getDate() < birthDate.getDate())
+  ) {
+    age--;
+  }
+
+  // Display the age in the "Introduction" section
+  document.getElementById("age").textContent = age + " years";
+}
 // Load header and footer components
 fetch("components/header.html")
   .then((response) => response.text())
@@ -7,6 +25,10 @@ fetch("components/header.html")
 fetch("components/footer.html")
   .then((response) => response.text())
   .then((data) => (document.getElementById("footer").innerHTML = data));
+
+fetch("components/hero.html")
+.then(response => response.text())
+.then(data => document.getElementById("hero").innerHTML = data);
 
 document.addEventListener("DOMContentLoaded", function () {
   // Load sidebar content
@@ -23,25 +45,48 @@ document.addEventListener("DOMContentLoaded", function () {
         sidebar.classList.toggle("d-none");
       });
 
-        // Load the default page
+      // Load the default page
       const page1Link = document.getElementById("page1Link");
+      const page2Link = document.getElementById("page2Link");
+      const page3Link = document.getElementById("page3Link");
+      const page4Link = document.getElementById("page4Link");
 
       const content = document.getElementById("content");
-  
+
       function loadContent(page) {
-          fetch(page)
-            .then((response) => response.text())
-            .then((data) => {
-              content.innerHTML = data;
-            });
-        }
-  
-        page1Link.addEventListener("click", function (e) {
-          e.preventDefault(); // Prevent the default link behavior
-          loadContent("pages/projects.html");
-          sidebar.classList.toggle("d-none");
-        });
+        fetch(page)
+          .then((response) => response.text())
+          .then((data) => {
+            content.innerHTML = data;
+            if (page === "pages/about.html") {
+              console.log("We are on age");
+              calculateAge();
+            } 
+          });
+      }
+
+      page1Link.addEventListener("click", function (e) {
+        e.preventDefault(); // Prevent the default link behavior
+        loadContent("pages/projects.html");
+        sidebar.classList.toggle("d-none");
+      });
+
+      page2Link.addEventListener("click", function (e) {
+        e.preventDefault(); // Prevent the default link behavior
+        loadContent("pages/about.html");
+        sidebar.classList.toggle("d-none");
+      });
+
+      page3Link.addEventListener("click", function (e) {
+        e.preventDefault(); // Prevent the default link behavior
+        loadContent("pages/contact.html");
+        sidebar.classList.toggle("d-none");
+      });
+
+      page4Link.addEventListener("click", function (e) {
+        e.preventDefault(); // Prevent the default link behavior
+        loadContent("pages/skills.html");
+        sidebar.classList.toggle("d-none");
+      });
     });
 });
-
-
